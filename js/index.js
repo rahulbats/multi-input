3113e3e6e1199b78e7033d6afaae942bfd02c076
Rahul Bhattacharya <rahulbhattacharya@Rahuls-MacBook-Air.local>
2017-02-05 21:23:12 -0600

    maxtags removed

--- js/index.js
+++ js/index.js
@@ -588,7 +588,7 @@
 		var id = $(obj).attr('id');
 		var tags = val.split(delimiter[id]);
 		for (i=0; i<tags.length; i++) {
-			$(obj).addTag(tags[i],{focus:false,callback:false,maxTags:options.maxTags});
+			$(obj).addTag(tags[i],{focus:false,callback:false});
 		}
 		if(tags_callbacks[id] && tags_callbacks[id]['onChange'])
 		{
